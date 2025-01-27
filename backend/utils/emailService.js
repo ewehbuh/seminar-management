842149aa46495a60f4b60efaac000a59fef158c0
Eweh-Emmanuel <emmanueleweh1@gmail.com>
2025-01-27 10:55:23 +0100

    'updated_port_and_email'

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = (to, subject, text) => {
   const mailOptions = {
-    from:process.env.EMAIL ,
+    from: process.env.EMAIL,
     to,
     subject,
     text,
